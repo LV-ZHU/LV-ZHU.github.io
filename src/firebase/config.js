184c5821b5f705cf1ca118e44cb0876cae1bb7e7
Hu ZhongPeng <2452487@tongji.edu.cn>
2026-05-05 23:56:30 +0800

    Delete src directory

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB2_9eQioXfA9KUQCup4lifzeVXU-L9U34",
-  authDomain: "my-personal-blog-f825c.firebaseapp.com",
-  projectId: "my-personal-blog-f825c",
-  storageBucket: "my-personal-blog-f825c.firebasestorage.app",
-  messagingSenderId: "1029767566489",
-  appId: "1:1029767566489:web:eb442d8c2506abfcab5408"
-}
