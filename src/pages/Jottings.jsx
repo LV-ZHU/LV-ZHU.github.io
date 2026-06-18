038b47b33a4cc5effff72d8772249822687809c2
LV-ZHU <2452487@tongji.edu.cn>
2026-06-18 12:30:59 +0800

    Remove sophomore schedule jotting

--- src/pages/Jottings.jsx
+++ src/pages/Jottings.jsx
@@ -4,7 +4,6 @@ import '../styles/Jottings.css'
 
 const articles = [
   { slug: 'jiqin-fenliu', title: '同济济勤巨类大一生存指北', date: '2025-7-16', tags: '学习 / 经验 / 分流' },
-  { slug: 'assignment', title: '大二下安排', date: '2026-3-20', tags: '前瞻' },
   { slug: 'interview', title: '面试合集', date: '2026-3-30', tags: '面试 / 答辩' },
 ]
 
